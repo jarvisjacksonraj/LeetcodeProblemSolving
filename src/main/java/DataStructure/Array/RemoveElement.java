f1219891fe2165d2ffc9881ba84583111ea71c84
jarvisjacksonraj <53148568+jarvisjacksonraj@users.noreply.github.com>
2021-08-28 00:15:04 +0530

    Remove Element modified.

--- src/main/java/DataStructure/Array/RemoveElement.java
+++ src/main/java/DataStructure/Array/RemoveElement.java
@@ -1,2 +1,19 @@
-package DataStructure.Array;public class RemoveElement {
+package DataStructure.Array;
+
+/**
+ * Array : Easy
+ * 27. Remove Element
+ */
+
+public class RemoveElement {
+    public int removeElement(int[] A, int elem) {
+        int j = 0;
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] != elem) {
+                A[j] = A[i];
+                j++;
+            }
+        }
+        return j;
+    }
 }
