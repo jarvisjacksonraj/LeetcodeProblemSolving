6867c096c858cc5b80a2e6b3d52193d397ab2e8a
jarvisjacksonraj <53148568+jarvisjacksonraj@users.noreply.github.com>
2021-08-28 07:30:42 +0530

    Merge Sorted Array.

--- src/main/java/DataStructure/Array/MergeSortedArray.java
+++ src/main/java/DataStructure/Array/MergeSortedArray.java
@@ -1,4 +1,21 @@
 package DataStructure.Array;
 
+/**
+ * Array : Easy
+ * 88. Merge Sorted Array
+ */
+
 public class MergeSortedArray {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int len1 = m - 1;
+        int len2 = n - 1;
+        int endIndex = nums1.length - 1;
+
+        while (len1 >= 0 && len2 >= 0) {
+            nums1[endIndex--] = (nums1[len1] > nums2[len2]) ? nums1[len1--] : nums2[len2--];
+        }
+        while (len2 >= 0) {
+            nums1[endIndex--] = nums2[len2--];
+        }
+    }
 }
