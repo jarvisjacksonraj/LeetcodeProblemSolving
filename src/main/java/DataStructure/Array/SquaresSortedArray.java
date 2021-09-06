6d8289c0005b2f3b494f867acf8555ba6e7ae191
jarvisjacksonraj <53148568+jarvisjacksonraj@users.noreply.github.com>
2021-09-06 18:14:06 +0530

    977. Squares of a Sorted Array

---
+++ src/main/java/DataStructure/Array/SquaresSortedArray.java
@@ -0,0 +1,27 @@
+package DataStructure.Array;
+
+/**
+ * Array: Easy
+ * 977. Squares of a Sorted Array
+ */
+
+public class SquaresSortedArray {
+    public int[] sortedSquares(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        int lastIndex = nums.length - 1;
+        int result[] = new int[nums.length];
+        while (start <= end) {
+            int startSqr = nums[start] * nums[start];
+            int endSqr = nums[end] * nums[end];
+            if (startSqr < endSqr) {
+                result[lastIndex--] = endSqr;
+                end--;
+            } else {
+                result[lastIndex--] = startSqr;
+                start++;
+            }
+        }
+        return result;
+    }
+}
